ffa6d1ad82d14937b00dca9ff026854256fb5b74
sromerop <sromerop@iesrafaelalberti.es>
2026-04-09 09:34:26 +0200

    Mejorando el metodo de control de fechas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,58 @@
 package org.iesra
 
 import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-fun main() {
+fun main(args: Array<String>) {
     val contenido = File("sample_app.log").readLines()
     var mensajeInfo = 0
     var mensajeWarning = 0
     var mensajeError = 0
 
+    var totalLineas = 0
+    var lineasValidas = 0
+    var lineasInvalidas = 0
+
+    var primeraFecha: LocalDateTime? = null
+    var ultimaFecha: LocalDateTime? = null
+
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    val fechaInicio = if (args.size >= 1) LocalDateTime.parse(args[0], formatter) else null
+    val fechaFin = if (args.size >= 2) LocalDateTime.parse(args[1], formatter) else null
+
     val regexFechaHora = Regex("""\[\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]) (?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d\]""")
 
     for (linea in contenido) {
+        totalLineas++
+
         val indiceCierre = linea.indexOf(']')
         if (indiceCierre != -1) {
             val fechaHora = linea.substring(0, indiceCierre + 1)
             val mensaje = linea.substring(indiceCierre + 1).trim()
 
             if (regexFechaHora.matches(fechaHora)) {
-                when {
-                    mensaje.contains("INFO") -> mensajeInfo++
-                    mensaje.contains("WARNING") -> mensajeWarning++
-                    mensaje.contains("ERROR") -> mensajeError++
+
+                val fechaTexto = fechaHora.removePrefix("[").removeSuffix("]")
+                val fecha = LocalDateTime.parse(fechaTexto, formatter)
+
+                val dentroRango =
+                    (fechaInicio == null || !fecha.isBefore(fechaInicio)) && (fechaFin == null || fechaFin.isAfter(
+                        fechaFin
+                    ))
+
+                if (dentroRango) {
+                    lineasValidas++
+
+                    when {
+                        mensaje.contains("INFO") -> mensajeInfo++
+                        mensaje.contains("WARNING") -> mensajeWarning++
+                        mensaje.contains("ERROR") -> mensajeError++
+                    }
+                } else {
+                    println("Formato inválido de fecha/hora en línea: $linea")
                 }
-            } else {
-                println("Formato inválido de fecha/hora en línea: $linea")
             }
         }
     }
