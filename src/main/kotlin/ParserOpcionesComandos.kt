2af879b708a8cbc9bbd7e80016f948c53867fce2
sromerop <sromerop@iesrafaelalberti.es>
2026-04-10 16:34:56 +0200

    Añadimos en el parser los comandos añadidos y los parseamos

--- src/main/kotlin/ParserOpcionesComandos.kt
+++ src/main/kotlin/ParserOpcionesComandos.kt
@@ -1,6 +1,12 @@
-package org.iesra
+import org.iesra.OpcionesComandos
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 class ParserOpcionesComandos {
+
+    private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
     fun parse(args: Array<String>): OpcionesComandos {
         val options = OpcionesComandos()
@@ -27,6 +33,32 @@ class ParserOpcionesComandos {
 
                 "-p", "--stdout" -> {
                     options.stdout = true
+                }
+
+                "-f", "--from" -> {
+                    i++
+                    if (i >= args.size) {
+                        throw IllegalArgumentException("Falta la fecha inicial")
+                    }
+
+                    options.from = try {
+                        LocalDateTime.parse(args[i], formatter)
+                    } catch (e: DateTimeParseException) {
+                        throw IllegalArgumentException("Formato de fecha inválido en --from")
+                    }
+                }
+
+                "-t", "--to" -> {
+                    i++
+                    if (i >= args.size) {
+                        throw IllegalArgumentException("Falta la fecha final")
+                    }
+
+                    options.to = try {
+                        LocalDateTime.parse(args[i], formatter)
+                    } catch (e: DateTimeParseException) {
+                        throw IllegalArgumentException("Formato de fecha inválido en --to")
+                    }
                 }
 
                 else -> {
