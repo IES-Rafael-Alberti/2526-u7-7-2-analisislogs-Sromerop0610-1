fc7066435651ecd19e31faf891a98b7064c92d3b
sromerop <sromerop@iesrafaelalberti.es>
2026-04-10 16:29:29 +0200

    Adaptamos el Main para que haga uso de los parser que hemos creado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,39 +6,33 @@ import java.time.format.DateTimeParseException
 
 fun main(args: Array<String>) {
 
-    if (args.isEmpty()) {
-        println("Uso: <fichero_log> [fecha_inicio] [fecha_fin] [fichero_salida]")
+    val parserCLI = ParserOpcionesComandos()
+
+    val options = try {
+        parserCLI.parse(args)
+    } catch (e: Exception) {
+        println(e.message)
         return
     }
 
-    val rutaFichero = args[0]
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-
-    val fechaInicio: LocalDateTime? = try {
-        if (args.size >= 2) LocalDateTime.parse(args[1], formatter) else null
-    } catch (e: DateTimeParseException) {
-        println("Formato de fecha_inicio incorrecto")
+    // VALIDACIONES BÁSICAS
+    if (options.input == null) {
+        println("Error: debes indicar --input")
         return
     }
 
-    val fechaFin: LocalDateTime? = try {
-        if (args.size >= 3) LocalDateTime.parse(args[2], formatter) else null
-    } catch (e: DateTimeParseException) {
-        println("Formato de fecha_fin incorrecto")
+    if (!options.stdout && options.output == null) {
+        println("Error: debes indicar --stdout o --output")
         return
     }
 
-    val ficheroSalida = if (args.size >= 4) args[3] else null
-
-    // Instanciar las clases
     val loader = LogFileLoader()
     val parser = LogParser()
     val analyzer = LogAnalyzer()
     val reporter = ReportGenerator()
 
-    // Cargar y parsear logs
     val lineas = try {
-        loader.loadLines(rutaFichero)
+        loader.loadLines(options.input!!)
     } catch (e: Exception) {
         println(e.message)
         return
@@ -46,12 +40,15 @@ fun main(args: Array<String>) {
 
     val entries = lineas.mapNotNull { parser.parseLine(it) }
 
-    // Analizar
-    analyzer.analyze(entries, fechaInicio, fechaFin)
+    analyzer.analyze(entries)
 
-    // Generar informe
     val report = reporter.generateReport(analyzer)
-    println(report)
 
-    ficheroSalida?.let { reporter.saveReport(it, report) }
+    if (options.stdout) {
+        println(report)
+    }
+
+    options.output?.let {
+        reporter.saveReport(it, report)
+    }
 }
