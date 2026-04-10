aee97f4f32115d2abc627f85e97b99914287c316
sromerop <sromerop@iesrafaelalberti.es>
2026-04-10 16:32:44 +0200

    Ampliamos las opciones de comandos

--- src/main/kotlin/OpcionesComandos.kt
+++ src/main/kotlin/OpcionesComandos.kt
@@ -1,7 +1,12 @@
 package org.iesra
 
+import java.time.LocalDateTime
+
 data class OpcionesComandos(
     var input: String? = null,
     var output: String? = null,
-    var stdout: Boolean = false
+    var stdout: Boolean = false,
+
+    var from: LocalDateTime? = null,
+    var to: LocalDateTime? = null
 )
